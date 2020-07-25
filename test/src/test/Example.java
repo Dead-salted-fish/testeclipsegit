205412a266b8d5bb49ef904cbfd54cf5f3413925
lld <lld1319366339@163.com>
2020-07-26 02:02:57 +0800

    测试eclispe的git

---
+++ test/src/test/Example.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Example{ 
+	String str=new String("good"); 
+	char[]ch={'a','b','c'}; 
+	
+	
+	
+	public void change(String str,char ch[]){ 
+	str="test ok"; 
+	ch[0]='g';
+	}
+	
+	}
---
+++ test/src/test/t.java
@@ -0,0 +1,15 @@
+package test;
+
+public class t {
+	
+	public static void main(String args[]){ 
+		String str=new String("good"); 
+		char[]ch={'a','b','c'}; 
+		
+	Example ex=new Example(); 
+	ex.change(str,ch); 
+	System.out.print(str+" and "); 
+	System.out.println(ch);
+	
+	} 
+}
